6eb1854a8f3895edb9f84264c970a10c6e808559
Lobanov Ivan <physic2003@mail.ru>
2024-02-27 03:19:38 +0300

    add service page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FC } from 'react'
+import ListServices from './pages/index.tsx'
+import ServicePage from './pages/service.tsx'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Header from './components/header/element.tsx'
+import Footer from './components/footer/element.tsx'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const StartPage: FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/labs-bmstu-2023-dia-frontend/" element={<ListServices />} />
+        <Route path='/labs-bmstu-2023-dia-frontend/services/:service_id/' element={<ServicePage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
-export default App
+export default StartPage
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: 3000,
+    proxy: {
+      '/api/v1': {
+        target: 'http://0.0.0.0:8000',
+        changeOrigin: true
+      }
+    }
+  },
+  base: "/labs-bmstu-2023-dia-frontend/",
   plugins: [react()],
 })
