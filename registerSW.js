ecb9b24b6acf77edf2a0fe10b12de1f856cb44f4
Lobanov Ivan <physic2003@mail.ru>
2024-02-29 18:01:16 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/labs-bmstu-2023-dia-frontend/sw.js', { scope: '/labs-bmstu-2023-dia-frontend/' })})}
